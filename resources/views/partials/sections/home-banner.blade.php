aa4399100a626fbc30b95cdaf5ca8057c0a00406
Chintan Dodiya <chinand.encircle@gmail.com>
2024-03-21 10:50:30 +0530

    21-03-2024

--- resources/views/partials/sections/home-banner.blade.php
+++ resources/views/partials/sections/home-banner.blade.php
@@ -1,24 +1,27 @@
-<section class="Home-banner">
-    <div class="content-wrap page-width">
-    
-            
-            @if( $content-> is_image_title)
-            @if( $content->title_image )
-                <img src="{{ $content->title_image }}" class="title-image" >
-            @endif
-                @else
-            @if( $content->title )
-                <h1 class="Title">{{ $content->title }}</h1>
-            @endif
-            @endif
-                <h2 class="sub-title">{{ $content->sub_title }}</h2>
-                <a href="{{ $content->button_url }}" class="banner-button font-bold">{{ $content->button_label }}</a>
-                <style>
-    section.Home-banner{
-        background-image: url('{{$content->background_image}}');
-    }
-</style>
-            
+<section class="banner-wrapper bg-White relative overflow-hidden h-[665px] lg:h-[650px] ">
+<div class="img absolute top-0 left-0 w-full h-full">
+            <img src="{{ $content -> background_image }}" class="img-fluid lozad" alt="herobanner" data-loaded="true">
+        </div>
+    <div class="banner-details relative h-full">
         
+        <div class="container-fluid h-full">
+            <div class="flex flex-col items-center justify-center w-full m-0 p-0 h-full text-center">
+                @if( $content-> is_image_title)
+                @if( $content->title_image )
+                <img src="{{ $content->title_image }}" class="title-image">
+                @endif
+                @else
+                @if( $content->title )
+                <h1 class="Title">{{ $content->title }}</h1>
+                @endif
+                @endif
+                <div class="title title-White font-Outfit mb-24 last:mb-0">
+                    <h1 class="h3">{{ $content->sub_title }}</h1>
+                </div>
+                <div class="btn-custom">
+                    <a href="{{ $content->button_url }}" class="btn btn-green">{{ $content->button_label }}</a> 
+                </div>
+            </div>
+        </div>
     </div>
 </section>
