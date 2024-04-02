1dfdd3e7f973c48c1bfb7c739d09cc6010daf846
Chintan Dodiya <chinand.encircle@gmail.com>
2024-04-02 12:31:51 +0530

    Color condition in sinle section code and added image for file

--- resources/views/partials/products/table.blade.php
+++ resources/views/partials/products/table.blade.php
@@ -1,13 +1,33 @@
-<section class="py-40 table-wrapper lg:py-80 ">
+@if ($content->background == 'white')
+    @php
+        $background_color_class = '';
+        $table_color_class = 'table-blue';
+        $title_color_class = 'title-Darkblue';
+    @endphp
+@elseif($content->background == 'aquamarine')
+    @php
+        $background_color_class = 'bg-Aquamarine';
+        $table_color_class = 'table-darkblue';
+        $title_color_class = 'title-White';
+    @endphp
+@elseif($content->background == 'blue')
+    @php
+        $background_color_class = 'bg-Blue-300';
+        $table_color_class = 'table-blue white';
+        $title_color_class = 'title-White';
+    @endphp
+@endif
+
+<section class="py-40 table-wrapper {!! $background_color_class !!} lg:py-80 ">
     <div class="container-fluid xl3:container">
         @foreach ($content->table as $item)
             <div class="relative flex flex-wrap items-center justify-center w-full p-0 m-0 mb-40 last:mb-0">
-                <div class="p-0 m-0 mb-20 text-center uppercase title title-Darkblue title-Redwing last:mb-0">
+                <div class="p-0 m-0 mb-20 text-center uppercase title {{ $title_color_class }} title-Redwing last:mb-0">
                     <h2>{!! $item['data']['title'] !!}</h2>
                 </div>
             </div>
             <div
-                class="relative flex flex-wrap items-center justify-center w-full gap-10 p-0 m-0 mb-40 table-blue last:mb-0">
+                class="relative flex flex-wrap items-center justify-center w-full gap-10 p-0 m-0 mb-40 {{ $table_color_class }} last:mb-0">
                 <table>
                     <thead>
                         <tr>
@@ -34,7 +54,20 @@
                                         @elseif($table['column_has'] == 'doc_link')
                                             @if (!empty($table['data'][$index]['file']))
                                                 <span>
-                                                    {!! $table['data'][$index]['file'] !!}
+                                                    <a href="{!! $table['data'][$index]['file']['url'] !!}" target="_blank"
+                                                        rel="noopener noreferrer">
+                                                        @if ($content->background == 'white')
+                                                            <img src="@asset('images/pdf-ext-dark.svg')"
+                                                                class="block h-auto max-w-full lozad">
+                                                        @elseif($content->background == 'aquamarine')
+                                                            <img src="@asset('images/pdf-ext.svg')"
+                                                                class="block h-auto max-w-full lozad">
+                                                        @elseif($content->background == 'blue')
+                                                            <img src="@asset('images/pdf-ext.svg')"
+                                                                class="block h-auto max-w-full lozad">
+                                                        @endif
+
+                                                    </a>
                                                 </span>
                                             @endif
                                         @elseif($table['column_has'] == 'link')
